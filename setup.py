48e0ec3ba0f5da36498d7da773b7d8615ad832d5
Arnout van Meer <rr2do2@gmail.com>
2018-06-28 16:23:19 +0100

    Updated setup.py to provide long description on PyPi

--- setup.py
+++ setup.py
@@ -1,15 +1,18 @@
+import io
 from setuptools import setup
 
+with io.open('README', 'rt', encoding='utf8') as f:
+    readme = f.read()
 
 setup(
     name='platter',
-    version='1.0',
-    url='http://github.com/mitsuhiko/platter/',
+    version='1.1',
+    url='https://github.com/mitsuhiko/platter/',
     license='BSD',
     author='Armin Ronacher',
     author_email='armin.ronacher@active-4.com',
     description='A deployment helper for Python.',
-    long_description=__doc__,
+    long_description=readme,
     py_modules=['platter'],
     platforms='any',
     install_requires=[
